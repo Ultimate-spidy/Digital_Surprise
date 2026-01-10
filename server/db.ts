0da07d1d9bd008edcf0ccdcf19af1c2b5a95c3f3
khushi singh <231210059@nitdelhi.ac.in>
2026-01-10 13:39:23 +0530

    Comment out DATABASE_URL validation check
    Comment out the DATABASE_URL error check.

--- server/db.ts
+++ server/db.ts
@@ -4,9 +4,9 @@ const { Pool } = pkg;
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL must be set. Did you forget to provision a database?");
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL must be set. Did you forget to provision a database?");
+// }
 
 export const pool = new Pool({
   connectionString: "mongodb+srv://231210059_db_user:Rashisingh27@ultimatespiderman.mxsmnas.mongodb.net/?appName=ultimateSpiderman",
